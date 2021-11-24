b78f629495f9deacce7ef34ef44670ceab95b19d
Stuart Woods <swoods91@gmail.com>
2021-11-24 19:17:21 +0000

    updating url

--- perf1.js
+++ perf1.js
@@ -52,7 +52,7 @@ export let options = {
 // Export a default function - this defines the entry point for your VUs,
 // similar to the main() function in many other languages.
 export default function () {
-    let res = http.get("https://sw-bloodpressureapp-qa-staging.azurewebsites.net/");
+    let res = http.get("https://sw-bloodpressureapp-qa-staging.azurewebsites.net");
 
     check(res, {
         "is status 200": (r) => r.status === 200
